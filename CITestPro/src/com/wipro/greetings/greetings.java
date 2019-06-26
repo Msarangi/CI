cb79c2eaef70ce7f2b089e6aca40e3f73db13fde
M Sarangi <M Sarangi@007bond.domain.name>
2019-06-27 01:04:52 +0530

    Greeting method and its main method

---
+++ CITestPro/src/com/wipro/greetings/greetings.java
@@ -0,0 +1,5 @@
+package com.wipro.greetings;
+
+public class greetings {
+
+}
---
+++ CITestPro/src/com/wipro/greetings/greetingsMain.java
@@ -0,0 +1,11 @@
+package com.wipro.greetings;
+
+public class greetingsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		greetings g=new greetings();
+		System.out.println(g.greet("Jenkins"));
+	}
+
+}
